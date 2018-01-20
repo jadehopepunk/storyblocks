f0fea4e0d24c5216e47de5e77c185a16a9ee6edc
Craig Ambrose <craig@craigambrose.com>
2018-01-20 21:38:11 +1100

    Some header layout

--- src/components/MessageHeader/index.js
+++ src/components/MessageHeader/index.js
@@ -3,24 +3,43 @@ import React from 'react'
 import styled from 'styled-jss'
 import Avatar from '../Avatar'
 import NameLink from '../NameLink'
+import PublishedAt from '../PublishedAt'
 
 type MessageHeaderProps = {
   author: {
     id: string,
     name: string
-  }
+  },
+  date: Date
 }
 
 const Container = styled('header')({
+  border: '1px solid blue',
+  display: 'flex',
+  flexDirection: 'row'
+})
+
+const Main = styled('div')({
+  flex: 1,
+  border: '1px solid blue',
+  marginLeft: 10,
+  marginRight: 10
+})
+
+const Meta = styled('div')({
   border: '1px solid red'
 })
 
-const MessageHeader = ({ author }: MessageHeaderProps) => (
+const MessageHeader = ({ author, date }: MessageHeaderProps) => (
   <Container>
     <Avatar name={author.name} />
-    <div className='name'>
+    <Main className='main'>
       <NameLink id={author.id} name={author.name} />
-    </div>
+      <PublishedAt date={date} />
+    </Main>
+    <Meta>
+      metadata
+    </Meta>
   </Container>
 )
 
